28a0a56645278962bbf80e9a90dfff349f183db0
radomir <3461408+radimd@users.noreply.github.com>
2024-01-07 17:09:12 -0600

    removed 'pi' user from poolpi.service; removed creation of venv

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-sudo -u $SUDO_UID BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; cd .. ; pwd -P)
-sudo -u $SUDO_UID echo "Setting up pool-pi."
-sudo -u $SUDO_UID echo "Creating venv."
-sudo -u $SUDO_UID pip3 install virtualenv
-sudo -u $SUDO_UID virtualenv ${BASEDIR}"/src/.venv"
-sudo -u $SUDO_UID source ${BASEDIR}"/src/.venv/bin/activate"
-sudo -u $SUDO_UID echo "Installing required python packages."
-sudo -u $SUDO_UID pip3 install -r ${BASEDIR}/setup/requirements.txt
-sudo -u $SUDO_UID echo "Configuring systemd."
+BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; cd .. ; pwd -P)
+
+echo "Setting up pool-pi."
+
+echo "Installing required python packages."
+pip3 install -r ${BASEDIR}/setup/requirements.txt
+
+echo "Configuring systemd."
+sed -e "s/#USER#/${SUDO_USER}/g" ${BASEDIR}/setup/poolpi.template > ${BASEDIR}/setup/poolpi.service
 cp ${BASEDIR}/setup/poolpi.service /etc/systemd/system/poolpi.service
 chmod 644 /etc/systemd/system/poolpi.service
 systemctl daemon-reload
 systemctl enable redis-server
 systemctl enable --now poolpi.service
-sudo -u $SUDO_UID echo "Setup script complete."
+
+echo "Setup script complete."
